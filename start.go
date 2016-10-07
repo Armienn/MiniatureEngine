6e23f4bda80234df502dced6ff53d68feb33f2a2
Armienn <nigathr@gmail.com>
2016-10-07 13:55:46 +0200

    Playing with arrays and splices

--- start.go
+++ start.go
@@ -8,4 +8,54 @@ func main() {
 	fmt.Print(b["moskus"])
 	b["moskus"] = 23
 	fmt.Print(b["moskus"])
+
+	var ints []int
+	arr := [...]int{1, 2, 3, 4, 5, 6, 7}
+	ints = arr[:]
+	fmt.Print("arr")
+	fmt.Println(arr)
+	fmt.Print("ints")
+	fmt.Println(ints)
+	ints[0] = 999
+	fmt.Print("arr")
+	fmt.Println(arr)
+	fmt.Print("ints")
+	fmt.Println(ints)
+	fmt.Println()
+
+	ints = ints[:5]
+	fmt.Print("arr")
+	fmt.Println(arr)
+	fmt.Print("ints")
+	fmt.Println(ints)
+	ints[2] = 999
+	fmt.Print("arr")
+	fmt.Println(arr)
+	fmt.Print("ints")
+	fmt.Println(ints)
+	fmt.Println()
+
+	ints = ints[:7]
+	fmt.Print("arr")
+	fmt.Println(arr)
+	fmt.Print("ints")
+	fmt.Println(ints)
+	ints[3] = 999
+	fmt.Print("arr")
+	fmt.Println(arr)
+	fmt.Print("ints")
+	fmt.Println(ints)
+	fmt.Println()
+
+	ints = append(ints, 23, 43, 23, 534)
+	fmt.Print("arr")
+	fmt.Println(arr)
+	fmt.Print("ints")
+	fmt.Println(ints)
+	ints[1] = 999
+	fmt.Print("arr")
+	fmt.Println(arr)
+	fmt.Print("ints")
+	fmt.Println(ints)
+	fmt.Println()
 }
